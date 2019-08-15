2600e5c3ac788c61e4075a96a855ee64a8ef9b63
Guillermo <you@example.com>
2019-08-15 21:12:30 +0200

    Primera prueba (todo junto)

--- import_data.py
+++ import_data.py
@@ -3,3 +3,9 @@ import numpy as np
 csv = np.genfromtxt('data/train.csv', delimiter=",")
 label = csv[1:, 0]
 data = csv[1:, 1:]
+
+M = len(data)
+data = np.append(data, 69 * np.ones((M, 1)), axis=1)
+
+print(data[1, 28*28])
+print(data[34, 28*28])
--- p1.py
+++ p1.py
@@ -2,9 +2,16 @@ import numpy as np
 from np import dot
 from math import exp
 
+# IMPORT DATA
+csv = np.genfromtxt('data/train.csv', delimiter=",")
+label = csv[1:, 0]
+data_sin_bias = csv[1:, 1:]
+bias = np.ones((M, 1))
+data = np.append(data, bias, axis=1)
+
 N0 = 28 * 28 + 1  # dimension layer 0
 N1 = 20  # dimension layer 1
-M = len(csv)  # dimension de data
+M = len(data)  # dimension de data
 
 
 def sigmoid(x):
@@ -40,14 +47,14 @@ def obtain_Ekt(label, Z, weights_capa1, weights_capa2):  # Ekt Matriz, asumiendo
     Ekt = np.zeros((M, 10))
     for k in range(M):
 	for t in range(10):
-	    zkt = 0
-	    if (label[k] == t) zkt = 1  # lo he cmabiado esto!!! creo q asi esta bien...
+            zkt = 0
+            if (int(label[k]) == t) zkt = 1  # lo he cmabiado esto!!! creo q asi esta bien...
 	    Ekt[k, t] = (Z[k, t] - zkt)
     return Ekt
 
 def calculate_error(Ekt):
-	return 0.5 * float(np.ones(M).dot(square_v(Ekt)).dot(np.ones(10).T))
-	
+    return 0.5 * float(np.ones(M).dot(square_v(Ekt)).dot(np.ones(10).T))
+
 """
 def obtain_Ekt(label, Z, weights_capa1, weights_capa2): #Ekt Matriz, asumiendo label vector fila de dimension M
 	Ekt = Z.copy()
@@ -61,14 +68,14 @@ def grad_capa2_jt(Y, weights_capa1, weights_capa2, j0, t0):
     derivada_jt = 0
     for k in range(M):
         derivada_jt += E[k,t0] * sigmoid_d(float(Y[k].dot(weights_capa2.T[t0].T))) * Y[k,j0]
-        return derivada_jt
+    return derivada_jt
 
 def grad_capa2(Y, weights_capa1, weights_capa2):
-	grad_weights_capa2 = np.zeros((N1, 10))
-	for j in range(N1):
-		for t in range(10):
-			grad_weights_capa2[j,t] = grad_capa2_jt(Y, weights_capa1, weights_capa2, j, t)
-	return grad_weights_capa2
+    grad_weights_capa2 = np.zeros((N1, 10))
+    for j in range(N1):
+	for t in range(10):
+            grad_weights_capa2[j,t] = grad_capa2_jt(Y, weights_capa1, weights_capa2, j, t)
+    return grad_weights_capa2
           
 def grad_capa1_ij(X, Y, weights_capa1, weights_capa2, i0, j0):
     M = len(data)
@@ -79,38 +86,40 @@ def grad_capa1_ij(X, Y, weights_capa1, weights_capa2, i0, j0):
     return derivada_ij
 
 def grad_capa1(X, Y, weights_capa1, weights_capa2):
-	grad_weights_capa1 = np.zeros((N1, 10))
-	for i in range(N1):
-		for j in range(10):
-			grad_weights_capa1[i,j] = grad_capa1_ij(X, Y, weights_capa1, weights_capa2, i, j)
-	return grad_weights_capa1
+    grad_weights_capa1 = np.zeros((N1, 10))
+    for i in range(N1):
+	for j in range(10):
+            grad_weights_capa1[i,j] = grad_capa1_ij(X, Y, weights_capa1, weights_capa2, i, j)
+
+    grad_weights_capa1[:, -1] = 0  # Forzar ceros en gradiente para el bias
+    return grad_weights_capa1
 
 def main():    
-    csv = np.genfromtxt('data/train.csv', delimiter=",")
-    label = csv[1:, 0]
-    data = csv[1:, 1:]
     weights_capa1 = np.zeros((N0, N1))
     weights_capa2 = np.zeros((N1, 10))
     Y = obtain_y(X, weights_capa1)
     Z = obtain_z(Y, weights_capa2)
     Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
 
-main()
+    epsilon = 1e-6
+    n_iteraciones = 5000
+    cont = 0
+    learning_rate = 0.01
 
-epsilon = 10e-6
-learning_rate = 0.01
-
-old_error = np.inf
-new_error = calculate_error(Ekt)
-
-while rel_error > eps:
-	new_weights_capa1 = weights_capa1 - learning_rate * grad_capa1(X, Y, weights_capa1, weights_capa2)
-	new_weights_capa2 = weights_capa2 - learning_rate * grad_capa2(Y, weights_capa1, weights_capa2)
-	
-	weights_capa1 = new_weights_capa1.copy()
-	weights_capa2 = new_weights_capa2.copy()
-    Y = obtain_y(X, weights_capa1)
-    Z = obtain_z(Y, weights_capa2)
-    Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
-    old_error = new_error
+    old_error = np.inf
     new_error = calculate_error(Ekt)
+    
+    while (rel_error > eps and cont < n_iteraciones):
+        cont += 1
+        new_weights_capa1 = weights_capa1 - learning_rate * grad_capa1(X, Y, weights_capa1, weights_capa2)
+        new_weights_capa2 = weights_capa2 - learning_rate * grad_capa2(Y, weights_capa1, weights_capa2)
+        weights_capa1 = new_weights_capa1.copy()
+        weights_capa2 = new_weights_capa2.copy()
+        Y = obtain_y(X, weights_capa1)
+        Z = obtain_z(Y, weights_capa2)
+        Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
+        old_error = new_error
+        new_error = calculate_error(Ekt)
+
+
+main()
