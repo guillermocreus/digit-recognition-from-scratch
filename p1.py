456531bd0cdc955815a928f1927ad76b28eb468b
Victor <vpicon98@gmail.com>
2019-08-18 13:36:34 +0200

    backpropagation version

--- p1.py
+++ p1.py
@@ -5,8 +5,8 @@ import time
 
 # IMPORT DATA
 csv = np.genfromtxt('data/train.csv', delimiter=",")
-label = csv[1:10, 0]
-data_sin_bias = csv[1:10, 1:]
+label = csv[1:-2, 0]
+data_sin_bias = csv[1:-2, 1:]
 M = len(data_sin_bias)  # dimension de data
 bias = np.ones((M, 1))
 X = np.append(data_sin_bias, bias, axis=1)
@@ -55,48 +55,50 @@ def obtain_Ekt(label, Z, weights_capa1, weights_capa2):  # Ekt Matriz, asumiendo
 
     return Ekt
 
+
 def calculate_error(Ekt):
     return 0.5 * float(np.ones(M).dot(square_v(Ekt)).dot(np.ones(10).T)) 
 
-"""
-def obtain_Ekt(label, Z, weights_capa1, weights_capa2): #Ekt Matriz, asumiendoo label vector fila de dimension M
-	Ekt = Z.copy()
+
+def obtain_delta_capa2(Z, Ekt):
+	delta_capa2 = np.zeros((M,10))
 	for k in range(M):
-		Ekt[k,label[k]] += -1
-    return Ekt
-"""
+		for t in range(10):
+		    delta_capa2[k,t] = Ekt[k,t] * sigmoid_d(float(Z[k,t]))
+	return delta_capa2
 
 
-def grad_capa2_jt(Y, Z, Ekt, weights_capa1, weights_capa2, j0, t0):
-    derivada_jt = 0
-    for k in range(M):
-        derivada_jt += Ekt[k,t0] * sigmoid_d(float(Z[k,t0])) * Y[k,j0]
-    return derivada_jt
-
-
-def grad_capa2(Y, Z, Ekt, weights_capa1, weights_capa2):
+def grad_capa2(Y, delta_capa2):
     grad_weights_capa2 = np.zeros((N1, 10))
     for j in range(N1):
         for t in range(10):
-            grad_weights_capa2[j, t] = grad_capa2_jt(Y, Z, Ekt, weights_capa1, weights_capa2, j, t)
+            grad = 0
+            for k in range(M):
+                grad += delta_capa2[k,t]*Y[k,j]
+            grad_weights_capa2[j, t] = grad
     return grad_weights_capa2
 
 
-def grad_capa1_ij(X, Y, Z, Ekt, weights_capa1, weights_capa2, i0, j0):
-    derivada_ij = 0
+def obtain_delta_capa1(Y, Ekt, weights_capa2, delta_capa2):
+    delta_capa1 = np.zeros((M,N1))
     for k in range(M):
-        for t in range(10):
-            derivada_ij += Ekt[k,t] * sigmoid_d(float(Z[k,t])) * weights_capa2[j0,t] * sigmoid_d(float(Y[k,j0])) * X[k,i0]
-    return derivada_ij
+        for j in range(N1):
+            delta = 0
+            for t in range(10):
+                delta += delta_capa2[k,t]*weights_capa2[j,t] #podriamos ponerlo como dot product
+            delta_capa1[k,j] = sigmoid_d(float(Y[k,j]))*delta
+    return delta_capa1
 
 
-def grad_capa1(X, Y, Z, Ekt, weights_capa1, weights_capa2):
+def grad_capa1(X, delta_capa1):
     grad_weights_capa1 = np.zeros((N0, N1))
     for i in range(N0):
         for j in range(N1):
-            grad_weights_capa1[i,j] = grad_capa1_ij(X, Y, Z, Ekt, weights_capa1, weights_capa2, i, j)
-
-    grad_weights_capa1[:, -1] = 0  # Forzar ceros en gradiente para el bias
+            grad = 0
+            for k in range(M):
+                grad += delta_capa1[k,j]*X[k,i]
+            grad_weights_capa1[i, j] = grad
+    grad_weights_capa1[:, -1] = 0  # Forzar ceros en gradiente para el bias CREO QUE ESTA MAL, ESTOY PENSANDO EN W transpuesta CREO _ REVISAR TODAS LAS W
     return grad_weights_capa1
 
 
@@ -118,8 +120,10 @@ def main():
         cont += 1
         print("buenas")
         start = time.time()
-        new_weights_capa1 = weights_capa1 - learning_rate * grad_capa1(X, Y, Z, Ekt, weights_capa1, weights_capa2)
-        new_weights_capa2 = weights_capa2 - learning_rate * grad_capa2(Y, Z, Ekt, weights_capa1, weights_capa2)
+        delta_capa2 = obtain_delta_capa2(Z, Ekt)
+        delta_capa1 = obtain_delta_capa1(Y, Ekt, weights_capa2, delta_capa2)
+        new_weights_capa2 = weights_capa2 - learning_rate * grad_capa2(Y, delta_capa2)
+        new_weights_capa1 = weights_capa1 - learning_rate * grad_capa1(X, delta_capa1)
         weights_capa1 = new_weights_capa1.copy()
         weights_capa2 = new_weights_capa2.copy()
         Y = obtain_y(X, weights_capa1)
