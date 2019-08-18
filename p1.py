3e63081f0ebd7a30d238b07dcfd4a095f363b333
Guillermo <you@example.com>
2019-08-18 15:10:37 +0200

    Parche para el overflow

--- p1.py
+++ p1.py
@@ -1,12 +1,11 @@
 import numpy as np
-from math import exp
-
 import time
 
 # IMPORT DATA
 csv = np.genfromtxt('data/train.csv', delimiter=",")
-label = csv[1:-2, 0]
-data_sin_bias = csv[1:-2, 1:]
+label = csv[1:, 0]
+data_sin_bias = csv[1:, 1:]
+data_sin_bias /= 783
 M = len(data_sin_bias)  # dimension de data
 bias = np.ones((M, 1))
 X = np.append(data_sin_bias, bias, axis=1)
@@ -16,7 +15,9 @@ N1 = 20  # dimension layer 1
 
 
 def sigmoid(x):
-    return 1.0/(1 + exp(-x))
+    if (x < -50):
+        return 1
+    return 1.0/(1 + np.exp(-x))
 
 
 def sigmoid_d(y):
@@ -50,43 +51,37 @@ def obtain_Ekt(label, Z, weights_capa1, weights_capa2):  # Ekt Matriz, asumiendo
         for t in range(10):
             zkt = 0
             if (int(label[k]) == t):
-                zkt = 1  # lo he cmabiado esto!!! creo q asi esta bien...
+                zkt = 1
             Ekt[k, t] = (Z[k, t] - zkt)
-
     return Ekt
 
 
 def calculate_error(Ekt):
-    return 0.5 * float(np.ones(M).dot(square_v(Ekt)).dot(np.ones(10).T)) 
+    aux = square_v(Ekt).dot(np.ones(10))
+    return 0.5 * float(np.ones(M).dot(aux))
 
 
 def obtain_delta_capa2(Z, Ekt):
-	delta_capa2 = np.zeros((M,10))
-	for k in range(M):
-		for t in range(10):
-		    delta_capa2[k,t] = Ekt[k,t] * sigmoid_d(float(Z[k,t]))
-	return delta_capa2
+    delta_capa2 = np.zeros((M, 10))
+    for k in range(M):
+        for t in range(10):
+            delta_capa2[k, t] = Ekt[k, t] * sigmoid_d(float(Z[k, t]))
+    return delta_capa2
 
 
 def grad_capa2(Y, delta_capa2):
     grad_weights_capa2 = np.zeros((N1, 10))
     for j in range(N1):
         for t in range(10):
-            grad = 0
-            for k in range(M):
-                grad += delta_capa2[k,t]*Y[k,j]
-            grad_weights_capa2[j, t] = grad
+            grad_weights_capa2[j, t] = delta_capa2[:, t].dot(Y[:, j])
     return grad_weights_capa2
 
 
 def obtain_delta_capa1(Y, Ekt, weights_capa2, delta_capa2):
-    delta_capa1 = np.zeros((M,N1))
+    delta_capa1 = np.zeros((M, N1))
     for k in range(M):
         for j in range(N1):
-            delta = 0
-            for t in range(10):
-                delta += delta_capa2[k,t]*weights_capa2[j,t] #podriamos ponerlo como dot product
-            delta_capa1[k,j] = sigmoid_d(float(Y[k,j]))*delta
+            delta_capa1[k, j] = sigmoid_d(float(Y[k, j])) * delta_capa2[k, :].dot(weights_capa2[j, :])
     return delta_capa1
 
 
@@ -94,17 +89,14 @@ def grad_capa1(X, delta_capa1):
     grad_weights_capa1 = np.zeros((N0, N1))
     for i in range(N0):
         for j in range(N1):
-            grad = 0
-            for k in range(M):
-                grad += delta_capa1[k,j]*X[k,i]
-            grad_weights_capa1[i, j] = grad
+            grad_weights_capa1[i, j] = delta_capa1[:, j].dot(X[:, i])
     grad_weights_capa1[:, -1] = 0  # Forzar ceros en gradiente para el bias CREO QUE ESTA MAL, ESTOY PENSANDO EN W transpuesta CREO _ REVISAR TODAS LAS W
     return grad_weights_capa1
 
 
-def main():    
-    weights_capa1 = np.zeros((N0, N1))
-    weights_capa2 = np.zeros((N1, 10))
+def main():
+    weights_capa1 = np.zeros((N0, N1))  # [i, j]
+    weights_capa2 = np.zeros((N1, 10))  # [j, t]
     Y = obtain_y(X, weights_capa1)
     Z = obtain_z(Y, weights_capa2)
     Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
@@ -132,10 +124,10 @@ def main():
         old_error = new_error
         new_error = calculate_error(Ekt)
         end = time.time()
-        print('rel Error = ' + str(rel_error(new_error, old_error))+',', 'elapsed time = '+str(end-start)+',', cont)	
+        print("stop")
+        print(old_error, new_error)
+        print('rel Error = ' + str(rel_error(new_error, old_error)) + ',', 'elapsed time = '+str(end-start)+',', cont)	
         
     print(weights_capa2[:,0])
-	
-
 
 main()
