47bace2c4f79ba6b927a90d99f1b7f8015891dff
Victor <vpicon98@gmail.com>
2019-08-22 11:57:11 +0200

    update binary bugs - aun no terminado

--- binary_neural.py
+++ binary_neural.py
@@ -28,16 +28,16 @@ class Neural_Network_Binary:
         self.M = len(label)
         self.digits = digits
         self.label = label
-        self.X = np.append(data, np.ones((self.M, 1)), axis=1)
-        self.N0 = len(self.X)
+        self.X = data
+        self.N0 = len(self.X[0])
         self.W = np.random.rand(self.N0, 1)
         
     def feed_forward(self):
         self.Z = sigmoid_v((self.X).dot(self.W))
         
     def obtain_Ekt(self):  # Ekt Matriz, asumiendo label vector fila de dimension M
-        Ekt = np.zeros((M, 1))
-        for k in range(M):
+        Ekt = np.zeros((self.M, 1))
+        for k in range(self.M):
             target = 0
             if (self.label[k] == self.digits[0]): 
                 target = 1
@@ -55,11 +55,11 @@ class Neural_Network_Binary:
         gradient = np.zeros((self.N0,1))
         for j in range(self.N0):
             for t in range(1):
-                gradient = delta[:, t].dot(X[:, j])
+                gradient = delta[:, t].dot(self.X[:, j])
                 
-	def calculate_error(self, Ekt):
-		aux = square_v(Ekt).dot(np.ones(1))
-		return 0.5 * float(np.ones(self.M).dot(aux))
+    def calculate_error(self, Ekt):
+        aux = square_v(Ekt).dot(np.ones(1))
+        return 0.5 * float(np.ones(self.M).dot(aux))
         
         
 def gradient_descend(NeuralNet, n_iteraciones, eps, learning_rate):
@@ -80,22 +80,33 @@ def gradient_descend(NeuralNet, n_iteraciones, eps, learning_rate):
     
     return NeuralNet    
 
+def main():
+    data, vM = import_labeled_data()
+    X = np.concatenate((data[1], data[1]), axis = 0) # v[r0] y v[s0] dan el numero de elementos existentes de ese label
+    label = vM[0]*[0] + vM[1]*[1]
+    NNB = Neural_Network_Binary(X, label, (0,1))
+    NNB = gradient_descend(NNB, 5000, 1e-4, 0.01)
+    print(NNB.W)
+
+'''
 
 def main():
     data, vM = import_labeled_data()
     iterator = combinations('0123456789', 2) #iterador con (0,1), (0,2), ... , (8,9)
     binary_nets = {} #diccionario con todos los classificadores binarios
     for digits in iterator:
-        d0 = int(digits[0])
-        d1 = int(digits[1])
-        X1 = A[d0]
-        X2 = A[d1]
+        digits = tuple(map(int,digits)) #pasar a integer
+        d0 = digits[0]
+        d1 = digits[1]
+        X1 = data[d0]
+        X2 = data[d1]
         X = np.concatenate((X1, X2), axis = 0) # v[r0] y v[s0] dan el numero de elementos existentes de ese label
         label = vM[d0]*[d0] + vM[d1]*[d1]
         NNB = Neural_Network_Binary(X, label, digits)
         NNB = gradient_descend(NNB, 5000, 1e-4, 0.01)
         binary_nets[digits] = NNB
+    
     return binary_nets
-        
-        
+'''
 
+main()
