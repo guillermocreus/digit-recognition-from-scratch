fcdbab1c0cd091365a897dc396e01aa0545501ad
guillecreus <guillermo.creus@basetis.com>
2019-08-30 10:49:58 +0200

    Revert "Correccion imports" Error mio (se han borrado archivos) This reverts commit 6eed4360bf42b9b282069ebb30a54151e1a09530.

---
+++ neural_network/classifier_neural_network.py
@@ -0,0 +1,163 @@
+import numpy as np
+import time
+import sys
+sys.path.insert(0, '../Import_data')
+from train_test_data import all_train_test_data
+
+
+# ________ IMPORT DATA ______________
+
+X, fotos_test, label, label_test = all_train_test_data()
+M = len(X)
+
+# ___________________________________
+
+
+N0 = 28 * 28 + 1  # dimension layer 0
+N1 = 20  # dimension layer 1
+
+
+def sigmoid(x):
+    return 1.0/(1 + np.exp(-x/30))
+    if (x < -50): return 0
+    elif (x > 50): return 1
+
+def sigmoid_d(y):
+    return y*(1-y) * (1/30)
+    
+def relu_mod(x):
+    if (x > 0): return x/100
+    else: return x/10000
+    
+def relu_d(y):
+    if (y > 0): return 1/100
+    else: return 1/10000
+
+def square(x):
+    return x*x
+
+
+sigmoid_v = np.vectorize(sigmoid)
+sigmoid_d_v = np.vectorize(sigmoid_d)
+relu_mod_v = np.vectorize(relu_mod)
+square_v = np.vectorize(square)
+
+
+def rel_error(new_error, old_error):
+    return abs(new_error - old_error) / new_error
+
+
+def obtain_y(X, weights_capa1):
+    res = relu_mod_v(X.dot(weights_capa1))
+    cont = 0
+    for k in range(M):
+        for j in range(N1):
+            if (res[k][j] < 5*1e-2 or (1 - res[k][j]) < 5*1e-2):
+                cont += 1
+    return res
+
+
+def obtain_z(Y, weights_capa2):
+    res = sigmoid_v(Y.dot(weights_capa2))
+    cont = 0
+    for k in range(M):
+        for t in range(10):
+            if (res[k][t] < 5*1e-2 or (1 - res[k][t]) < 5*1e-2):
+                cont += 1
+    print("% Neuronas saturadas (Z):")
+    print(100.0 * cont / (10 * M))
+    return res
+
+
+def obtain_Ekt(label, Z, weights_capa1, weights_capa2):  # Ekt Matriz, asumiendo label vector fila de dimension M
+    Ekt = np.zeros((M, 10))
+    for k in range(M):
+        for t in range(10):
+            zkt = 0
+            if (int(label[k]) == t):
+                zkt = 1
+            Ekt[k, t] = (Z[k, t] - zkt)
+    return Ekt
+
+
+def calculate_error(Ekt):
+    aux = square_v(Ekt).dot(np.ones(10))
+    return 0.5 * float(np.ones(M).dot(aux))
+
+
+def print_precision():
+    
+    print()
+
+def obtain_delta_capa2(Z, Ekt):
+    delta_capa2 = np.zeros((M, 10))
+    for k in range(M):
+        for t in range(10):
+            delta_capa2[k, t] = Ekt[k, t] * sigmoid_d(float(Z[k, t]))
+    return delta_capa2
+
+
+def grad_capa2(Y, delta_capa2):
+    grad_weights_capa2 = np.zeros((N1, 10))
+    for j in range(N1):
+        for t in range(10):
+            grad_weights_capa2[j, t] = delta_capa2[:, t].dot(Y[:, j])
+    return grad_weights_capa2
+
+
+def obtain_delta_capa1(Y, Ekt, weights_capa2, delta_capa2):
+    delta_capa1 = np.zeros((M, N1))
+    for k in range(M):
+        for j in range(N1):
+            delta_capa1[k, j] = relu_d(float(Y[k, j])) * delta_capa2[k, :].dot(weights_capa2[j, :])
+    return delta_capa1
+
+
+def grad_capa1(X, delta_capa1):
+    grad_weights_capa1 = np.zeros((N0, N1))
+    for i in range(N0):
+        for j in range(N1):
+            grad_weights_capa1[i, j] = delta_capa1[:, j].dot(X[:, i])
+    return grad_weights_capa1
+
+
+def main():
+    weights_capa1 =  np.random.rand(N0, N1)  # [i, j]
+    weights_capa2 =  np.random.rand(N1, 10)  # [j, t]
+    Y = obtain_y(X, weights_capa1)
+    Y[:, -1] = 1
+    Z = obtain_z(Y, weights_capa2)
+    Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
+    eps = 1e-4
+    n_iteraciones = 100
+    cont = 0
+    learning_rate = 0.1
+    print(label[1])
+    print(label[2])
+    old_error = np.inf
+    new_error = calculate_error(Ekt)
+    
+    while (cont < n_iteraciones):
+        cont += 1
+        if (cont % 100 == 0): print_precision(weights_capa1, weights_capa2)
+
+        start = time.time()
+        delta_capa2 = obtain_delta_capa2(Z, Ekt)
+        delta_capa1 = obtain_delta_capa1(Y, Ekt, weights_capa2, delta_capa2)
+        weights_capa2 -= learning_rate * grad_capa2(Y, delta_capa2)
+        weights_capa1 -= learning_rate * grad_capa1(X, delta_capa1)
+        Y = obtain_y(X, weights_capa1)
+        Y[:, -1] = 1
+        Z = obtain_z(Y, weights_capa2)
+        Ekt = obtain_Ekt(label, Z, weights_capa1, weights_capa2)
+        old_error = new_error
+        new_error = calculate_error(Ekt)
+        end = time.time()
+        print("stop")
+        print(old_error, new_error)
+        print('rel Error = ' + str(rel_error(new_error, old_error)) + ',', 'elapsed time = '+str(end-start)+',', cont)
+        print()
+    print(Z[0:5, :])
+    print(label[0:5])
+
+main()
---
+++ neural_network/train_binary_classifier.py
@@ -0,0 +1,172 @@
+import numpy as np
+import time
+from numpy.linalg import inv
+import sys
+from itertools import combinations
+sys.path.insert(0, '../Import_data')
+from train_test_data import train_test_data
+
+A_train, A_test, label_train, label_test, v_train, v_test = train_test_data()
+
+N0 = 28 * 28 + 1  # dimension layer 0
+M = 0;  # Se actualiza posteriormente
+
+
+def sigmoid(x):
+    return 1.0/(1 + np.exp(-x/30))    
+
+
+def sigmoid_d(y):
+    return y*(1-y)*1/30
+
+
+def square(x):
+    return x*x
+
+
+sigmoid_v = np.vectorize(sigmoid)
+sigmoid_d_v = np.vectorize(sigmoid_d)
+square_v = np.vectorize(square)
+
+
+def rel_error(new_error, old_error):
+    return abs(new_error - old_error) / new_error
+
+
+def obtain_y(X, weights):
+    return sigmoid_v(X.dot(weights))
+
+
+def obtain_Ek(X, Y, separador, d0, d1):  # Ek Matriz, asumiendo label vector fila de dimension M
+    M = len(X)
+    Ek = np.zeros(M)
+    for k in range(M):
+        zk = 0
+        if (k >= separador):
+            zk = 1
+        Ek[k] = (Y[k] - zk)
+    return Ek
+
+
+def calculate_error(Ek, X):
+    M = len(X)
+    aux = square_v(Ek)
+    return 0.5 * float(np.ones(M).dot(aux))
+
+
+def hessian(X, Y, Ek):
+    M = len(X)
+    Y_d = sigmoid_d_v(Y)
+    H = np.zeros((785, 785))
+    for k in range(M):
+        for i in range(785):
+            for j in range(785):
+                if (i <= j):
+                    H[i, j] += Y_d[k] * X[k][i] * X[k][j] * (Y_d[k] + Ek[k] * (1 - 2 * Y[k]))
+
+        print("Acabo foto " + str(k))
+
+    for i in range(785):
+        for j in range(785):
+            if (i > j):
+                H[i, j] = H[j, i]
+
+    return H
+
+def grad(X, Y, Ek):
+    M = len(X)
+    grad_weights = np.zeros(N0)
+    Y_d = sigmoid_d_v(Y)
+    for i in range(N0):
+        aux = 0
+        for k in range(M):
+            aux += Ek[k]*X[k][i]*Y_d[k]
+        grad_weights[i] = aux
+    return grad_weights
+
+
+# Entrena el clasificador binario de labels d0, d1 y devuelve los pesos
+# Supongo que el output correcto es d1 --> output = 1
+def train_classifier(X, separador, d0, d1):
+    M = len(X)
+    weights = np.random.rand(N0)  # [i, t]
+    Y = obtain_y(X, weights)
+    Ek = obtain_Ek(X, Y, separador, d0, d1)
+    eps = 1e-5
+    n_iteraciones = 3
+    cont = 0
+    learning_rate = 0.01
+    old_error = np.inf
+    new_error = calculate_error(Ek, X)
+
+    while (cont < n_iteraciones): #and rel_error(new_error, old_error) > eps):
+        print("inside")
+        cont += 1
+        start = time.time()
+        gradiente = grad(X, Y, Ek)
+        Hessian = hessian(X, Y, Ek)
+        print("hessiana acabada")
+        weights -= learning_rate * inv(Hessian).dot(gradiente)
+        #weights -= learning_rate * gradiente
+        Y = obtain_y(X, weights)
+        Ek = obtain_Ek(X, Y, separador, d0, d1)
+        old_error = new_error
+        new_error = calculate_error(Ek, X)
+        end = time.time()
+        print(old_error, new_error)
+        print('Training ' + str(d0) + ' vs ' + str(d1) + ': -->', 'rel Error = ' + str(rel_error(new_error, old_error)) + ',', 'elapsed time = '+str(end-start)+',', cont, '\n')
+        print(Y)
+
+    return weights
+
+
+def test_data(binary_nets, foto):
+    v = 10 * [0]
+    for digits in binary_nets:
+        result = float(sigmoid(weights.dot(foto)))
+        if result >= .5:
+            v[digits[1]] += 1
+        else:
+            v[digits[0]] += 1
+    return np.argmax(v)
+    
+
+def train_binary_nets():
+    iterator = combinations('0123456789', 2)  # iterador con (0,1), (0,2), ... , (8,9)
+    binary_nets = {}  # diccionario con todos los classificadores binarios
+
+    for digits in iterator:
+
+        digits = tuple(map(int,digits)) #pasar a integer
+        d0 = digits[0]
+        d1 = digits[1]
+        X1 = A_train[d0]
+        X2 = A_train[d1]
+        X = np.concatenate((X1, X2), axis = 0)
+        separador = len(X1)
+        weights = train_classifier(X, separador, d0, d1)
+        
+        name = 'saved_weights/weights' + str(d0) + 'vs' + str(d1) + '.csv'
+        np.savetxt(name, weights, delimiter=",")
+        binary_nets[digits] = weights
+
+    
+    return binary_nets
+    
+def main ():
+    binary_nets = train_binary_nets()   
+    print("End training")
+     
+    N_aciertos = 0
+    N_test = len(label_test)
+    precision = N_aciertos/N_test
+    for i in range(10):
+        print(i)
+        for foto in A_test[i]:
+            prediction = test_data(binary_nets, foto)
+            if (prediction == i): N_aciertos += 1
+        print("Precision en digito ", i, " : ", precision) 
+    print("Precision Final: ", precision)
+
+
+main()
